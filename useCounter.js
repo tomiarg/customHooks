05867eb6957340ff5aa3098842b5cfdaff610b6c
Tomas <tomimoyanook@gmail.com>
2023-08-13 16:41:30 -0300

    primercommit

---
+++ useCounter.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export const useCounter = (initialValue = 10) => {
+    const [ counter, setCounter] = useState(initialValue)
+    
+    const increment =()=>{
+        setCounter((current)=>current + 1)
+    }
+    const decrement =()=>{
+        setCounter(counter - 1)
+    }
+    
+    const reset =()=>{
+        setCounter( initialValue)
+    }
+  return {
+    counter,
+    increment,
+    decrement,
+    reset
+  } 
+  
+}
---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState=[]
+
+const init = () =>{
+   return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init );
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))    
+    }, [todos])
+    
+    const onHandleTodo = (todo) =>{
+    
+        const action ={
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+    
+        dispatch(action)
+    
+    }
+
+    const HandleDeleteTodo =(id)=>{
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+
+            }
+        )        
+    }
+    const handleToggleTodo =(id)=>{
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+            }
+        )      
+    }
+    const todosCount = todos.length
+    const pendingTodosCount = todos.filter(todo=>!todo.done).length
+ 
+
+  return {
+    todos,
+    onHandleTodo,
+    HandleDeleteTodo,
+    handleToggleTodo,
+    todosCount,
+    pendingTodosCount
+
+  }
+}
